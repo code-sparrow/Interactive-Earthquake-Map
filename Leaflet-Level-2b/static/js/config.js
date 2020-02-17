0f06f4487089f13fffbb35770e81ac2bbe1fed37
code-sparrow <code.sparrow7@gmail.com>
2020-02-17 01:49:41 -0500

    added grouped layer control with timespan to level2b

---
+++ Leaflet-Level-2b/static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+//const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiY29kZS1zcGFycm93IiwiYSI6ImNrNjZ1d2VocDE2c2EzZXFvc2VseGlrNXgifQ.jgjpRvSEA-k0CoxyfRY0oA";
--- Leaflet-Level-2b/static/js/logic.js
+++ Leaflet-Level-2b/static/js/logic.js
@@ -1,46 +1,62 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var queryUrlHour = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+var queryUrlDay = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+var queryUrlWeek = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var queryUrlMonth = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 var faultUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-    d3.json(faultUrl, function(data2) {
-    // Once we get a response, send the data.features object to the createFeatures function
-        console.log(data);
-        console.log(data2);
-        createFeatures(data.features, data2.features);
+
+d3.json(queryUrlHour, function(dataHour) {
+    d3.json(queryUrlDay, function(dataDay) {
+        d3.json(queryUrlWeek, function(dataWeek) {
+            d3.json(queryUrlMonth, function(dataMonth) {
+                d3.json(faultUrl, function(dataFault) {
+                    var data = [dataHour.features, dataDay.features, dataWeek.features, dataMonth.features];
+                    //console.log(data);
+                    createFeatures(data, dataFault.features);
+                });
+            });
+        });
     });
 });
 
-
-
 function createFeatures(earthquakeData, faultData) {
     // reorder data based on magnitude so smaller circles are always drawn on top of larger circles
-    earthquakeData = earthquakeData.slice().sort((a, b) => d3.descending(a.properties.mag, b.properties.mag));
-
+    newEQData = [];
+    earthquakeData.forEach(span => {
+        newEQData.push(
+        span.slice().sort((a, b) => d3.descending(a.properties.mag, b.properties.mag))
+        )
+    });
+    //console.log(newEQData);
     // An array which will be used to store created earthquakeMarkers
     var earthquakeMarkers = [];
+    
     var faultMarkers = [];
 
     // for each feature in the earthquakeData array, add circle to marker array
-    earthquakeData.forEach(feature => {
-        // coordinates lat/lng, for circle center
-        var coord = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
-        //console.log(feature.properties.mag, feature.properties.place, Date(feature.properties.time));
-        // push it to the earthquakeMarkers array
-        earthquakeMarkers.push(
-            L.circle(coord, {
-                fillOpacity: 0.65,
-                color: "#563E04",
-                weight: 0.7,
-                fillColor: c_color(feature.properties.mag),
-                // Adjust radius
-                radius: feature.properties.mag * 50000
-            }).bindPopup(`<h3>${feature.properties.place}</h3><hr>
-            <p>${new Date(feature.properties.time)}</p><hr>
-            <p><b>Magnitude: &nbsp </b> ${feature.properties.mag}</p>`)
-        )
+    newEQData.forEach(span => {
+        var eqMarkTemp = [];
+        span.forEach(feature => {
+            var coord = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
+            eqMarkTemp.push(
+                L.circle(coord, {
+                    fillOpacity: 0.65,
+                    color: "#563E04",
+                    weight: 0.7,
+                    fillColor: c_color(feature.properties.mag),
+                    // Adjust radius
+                    radius: feature.properties.mag * 50000
+                }).bindPopup(`<h3>${feature.properties.place}</h3><hr>
+                <p>${new Date(feature.properties.time)}</p><hr>
+                <p><b>Magnitude: &nbsp </b> ${feature.properties.mag}</p>`)
+            )
+        })
+        earthquakeMarkers.push(eqMarkTemp);
     });
+
+    //console.log(earthquakeMarkers);
 
     faultData.forEach(feature => {
 
@@ -60,7 +76,12 @@ function createFeatures(earthquakeData, faultData) {
 
     // Add all the earthquakeMarkers to a new layer group.
     // Now we can handle them as one group instead of referencing each individually
-    var markerLayer = L.layerGroup(earthquakeMarkers);
+    var markerLayer = [];
+    earthquakeMarkers.forEach(span => {
+        markerLayer.push(L.layerGroup(span));
+    });
+    console.log(markerLayer);
+    //var markerLayer = L.layerGroup(earthquakeMarkers);
     var markerLayer2 = L.layerGroup(faultMarkers);
 
     createMap(markerLayer, markerLayer2);
@@ -107,9 +128,26 @@ function createMap(earthquakes, faults) {
     };
 
     // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-        Earthquakes: earthquakes,
-        Faults: faults
+    //var overlayMaps = {
+    //   Faults: faults
+    //};
+
+
+
+    var groupedOverlays = {
+        "Time Span": {
+            "Past Hour": earthquakes[0],
+            "Past Day": earthquakes[1],
+            "Past Week": earthquakes[2],
+            "Past Month": earthquakes[3]
+        }
+    };
+    var options = {
+        // Make the "Time Span" group exclusive (use radio inputs)
+        exclusiveGroups: ["Time Span"],
+        // Show a checkbox next to non-exclusive group labels for toggling all
+        groupCheckboxes: true,
+        collapsed: false
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -119,15 +157,19 @@ function createMap(earthquakes, faults) {
         ],
         zoom: 4,
         // fault and earthquake overlays checked by default
-        layers: [graymap, faults, earthquakes]
+        layers: [graymap, faults, earthquakes[2]]
     });
 
     // Create a layer control
     // Pass in our baseMaps and overlayMaps
     // Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
+    // L.control.layers(baseMaps, overlayMaps, {
+    //     collapsed: false
+    // }).addTo(myMap);
+
+    var layerControl = L.control.groupedLayers(baseMaps, groupedOverlays, options);
+    layerControl.addOverlay(faults, "Faults");
+    myMap.addControl(layerControl);
 
 
     //------------------------------------
@@ -158,6 +200,7 @@ function createMap(earthquakes, faults) {
     legend.addTo(myMap);
 };
 
+//--------------------------------------------------------
 // Color function for feature magnitude
 function c_color(mag) { 
 
@@ -180,3 +223,22 @@ function c_color(mag) {
         return "#28CA03";
     }
 };
+
+// //-----------------------------------------------
+// const buttons = d3.selectAll('input');
+// buttons.on('change', function(d) {
+//     if (this.value === "hour") {
+//         queryData(queryUrlHour, faultUrl);
+//     } 
+//     else if (this.value === "day") {
+//         queryData(queryUrlDay, faultUrl);
+//     }
+//     else if (this.value === "month") {
+//         queryData(queryUrlMonth, faultUrl);
+//     }
+//     else {
+//         queryData(queryUrlWeek, faultUrl);
+//     }
+    
+// });
+
